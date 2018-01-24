74b5ae6a5565965650d00421746d1e9d639612ee
artikrik <artikrik@gmail.com>
2018-01-24 20:52:34 +0200

    added colors and fixes

--- install-scripts/step1.sh
+++ install-scripts/step1.sh
@@ -1,13 +1,12 @@
-#!bin/bash
-echo '##### Installing updates and install soft...'
+#! /bin/bash
+echo -e '\033[0;32m##### Installing updates and install soft...\033[0;32m'
 sudo apt-get install -y git && sudo apt-get install -y openssh-server && sudo apt-get install -y screen && sudo apt-get install -y htop
-echo '#### Xorg server disabling for the next sessions... (to enable use command [systemctl set-default graphical.target])'
+echo -e '\033[0;32m##### Xorg server disabling for the next sessions... (to enable use command [systemctl set-default graphical.target])\033[0;32m'
 sleep 5
 sudo systemctl set-default multi-user.target
-echo '#### Xorg server disabling for the next sessions... (to enable use command [systemctl set-default graphical.target])'
 sleep 5
 sudo killall /usr/bin/X
 sudo service lightdm stop
-echo 'Please make sure the system sees all GPUs...'
+echo -e '\033[0;32m##### Please make sure the system sees all GPUs...\033[0;32m'
 sleep 5
-printf "$(lspci -v | grep VGA)\n"
+printf "\033[0;32m VGA CARD $(lspci -v | grep VGA)\n\033[0;32m"
